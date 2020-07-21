7ee60c5087e182a0ad0fc107663845177c5b7a24
bocanegra01 <jose.bocanegra.cbtis195.a@gmail.com>
2020-07-21 14:18:28 -0500

    Log in

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -3,11 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
-    /**
-     * This method is used to load the login.php view.
-     */
-	public function index()
-	{
+	public function index() {
 		$this->load->view('login');
+	}
+
+	public function validateFormData() {
+		
 	}
 }
