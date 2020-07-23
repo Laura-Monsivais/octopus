b6f19f6b10aafc7ccbf8676f6a622fba0f091e0d
bocanegra01 <jose.bocanegra.cbtis195.a@gmail.com>
2020-07-23 18:31:15 -0500

    Template

--- application/views/MainView.php
+++ application/views/MainView.php
@@ -1,5 +1,5 @@
 <div class="page-wrapper">
     <div class="container-fluid">
-    ¡Hola diseañores¡ Esta es la pantalla principal. 
+    ¡Hola diseañores! Esta es la pantalla principal. 
     </div>
 </div>
