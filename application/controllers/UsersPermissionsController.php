6010f930f673f81e7200974ef7731dbd5a926cbe
bocanegra01 <jose.bocanegra.cbtis195.a@gmail.com>
2020-07-23 18:55:57 -0500

    Users and permissions template

--- application/controllers/UsersPermissionsController.php
+++ application/controllers/UsersPermissionsController.php
@@ -1,13 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MainController extends CI_Controller {
+class UsersPermissionsController extends CI_Controller {
 
 	public function index() {
 		$this->load->view("components/LoaderComponent");
 		$this->load->view("components/HeaderComponent");
 		$this->load->view("components/NavbarComponent");
-		$this->load->view("MainView");
+		$this->load->view("UsersPermissionsView");
 		$this->load->view("components/FooterComponent");
 	}
 }
