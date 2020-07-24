56fbf31ec17e75c883d30ea4c2438f647311dfc5
bocanegra01 <jose.bocanegra.cbtis195.a@gmail.com>
2020-07-23 22:19:23 -0500

    Past

--- application/views/ProviderView.php
+++ application/views/ProviderView.php
@@ -1,10 +1,5 @@
 <div class="page-wrapper">
     <div class="container-fluid">
-        <form>
-            <div class="customize-input">
-                <input class="form-control custom-shadow custom-radius border-0 bg-white"
-                    type="search" placeholder="Search" aria-label="Search">
-            </div>
-        </form>
+        
     </div>
 </div>
