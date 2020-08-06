0c218ac3fd90a75078315e7d06959c80e7fcc7ef
bocanegra01 <jose.bocanegra.cbtis195.a@gmail.com>
2020-08-06 03:22:29 -0500

    FIX BUGS

--- application/views/components/NavbarComponent.php
+++ application/views/components/NavbarComponent.php
@@ -1,4 +1,3 @@
-
 <aside class="left-sidebar" data-sidebarbg="skin6">
     <div class="scroll-sidebar" data-sidebarbg="skin6">
         <nav class="sidebar-nav">
@@ -8,10 +7,12 @@
                             class="hide-menu">Proveedores</span></a>
                     <ul aria-expanded="false" class="collapse  first-level base-level-line">
                         <li class="sidebar-item">
-                            <a href="<?php base_url()?>ProviderController" class="sidebar-link"><span class="hide-menu">Nuevo proveedor</span></a>
+                            <a href="<?php base_url()?>ProviderController" class="sidebar-link"><span
+                                    class="hide-menu">Nuevo proveedor</span></a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="<?php base_url()?>RawMaterialController" class="sidebar-link"><span class="hide-menu">Insumos</span></a>
+                            <a href="<?php base_url()?>RawMaterialController" class="sidebar-link"><span
+                                    class="hide-menu">Insumos</span></a>
                         </li>
                     </ul>
                 </li>
@@ -21,14 +22,8 @@
                         aria-expanded="false"><i data-feather="file-text" class="feather-icon"></i><span
                             class="hide-menu">Equipos técnicos</span></a>
                     <ul aria-expanded="false" class="collapse  first-level base-level-line">
-                    <li class="sidebar-item">
+                        <li class="sidebar-item">
                             <a href="" class="sidebar-link"><span class="hide-menu">Nuevo equipo</span></a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="" class="sidebar-link"><span class="hide-menu">Tabla de equipos</span></a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="" class="sidebar-link"><span class="hide-menu">Mantenimiento</span></a>
                         </li>
                     </ul>
                 </li>
@@ -37,7 +32,8 @@
                             class="hide-menu">Servicios</span></a>
                     <ul aria-expanded="false" class="collapse  first-level base-level-line">
                         <li class="sidebar-item">
-                            <a href="<?php base_url()?>ServiceController" class="sidebar-link"><span class="hide-menu">Nuevo servicio</span></a>
+                            <a href="<?php base_url()?>ServiceController" class="sidebar-link"><span
+                                    class="hide-menu">Nuevo servicio</span></a>
                         </li>
                     </ul>
                 </li>
@@ -46,7 +42,8 @@
                             class="hide-menu">Calendario</span></a>
                     <ul aria-expanded="false" class="collapse  first-level base-level-line">
                         <li class="sidebar-item">
-                            <a href="<?php base_url()?>CalendarController" class="sidebar-link"><span class="hide-menu">Nuevo evento</span></a>
+                            <a href="<?php base_url()?>CalendarController" class="sidebar-link"><span
+                                    class="hide-menu">Nuevo evento</span></a>
                         </li>
                     </ul>
                 </li>
@@ -55,7 +52,8 @@
                             class="hide-menu">Empleados</span></a>
                     <ul aria-expanded="false" class="collapse  first-level base-level-line">
                         <li class="sidebar-item">
-                            <a href="<?php base_url()?>EmployeeController" class="sidebar-link"><span class="hide-menu">Nuevo empleado</span></a>
+                            <a href="<?php base_url()?>EmployeeController" class="sidebar-link"><span
+                                    class="hide-menu">Nuevo empleado</span></a>
                         </li>
                     </ul>
                 </li>
