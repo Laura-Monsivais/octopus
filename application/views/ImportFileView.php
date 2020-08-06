cbd1e64379b64554afed9e96c82b2cfc7c17601a
bocanegra01 <jose.bocanegra.cbtis195.a@gmail.com>
2020-08-06 02:23:44 -0500

    FIX BUGS

--- application/views/ImportFileView.php
+++ application/views/ImportFileView.php
@@ -35,32 +35,35 @@
                     </div>
                 </form>
             </div>
-        </form>
-    </div>
-</div>
-		<table class="table">
-			<thead>
-				<tr>
-					<th scope="col">Nombre</th>
-					<th scope="col">Link</th>
-					<th scope="col">Tipo de doumento</th>
-					<th scope="col">Acción</th>
-				</tr>
-			</thead>
-			<tbody>
-			
-			<?php 
-				$archivos = json_decode(json_encode($listaArchivos), true);
-				$path = site_url();
-				foreach ($archivos as $archivo) {
-					echo "<tr >";
-					echo "<td >" . $archivo["nombre_archivo"] . "</td>";
-					echo "<td >" . $archivo["ruta"] . "</td>";
-					echo "<td >" . $archivo["categoria"] . "</td>";
-                    echo "<td ><a class='btn btn-primary' href='${path}/ImportFileController/descargarArchivo/" . $archivo["ruta"] . "'>Descargar</a></td>"; 
-					echo "</tr>";
-				}
-			?>
-			
-			</tbody>
-		</table>
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Link</th>
+                            <th scope="col">Tipo de doumento</th>
+                            <th scope="col">Acción</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    
+                    <?php 
+                        $archivos = json_decode(json_encode($listaArchivos), true);
+                        $path = site_url();
+                        foreach ($archivos as $archivo) {
+                            echo "<tr >";
+                            echo "<td >" . $archivo["nombre_archivo"] . "</td>";
+                            echo "<td >" . $archivo["ruta"] . "</td>";
+                            echo "<td >" . $archivo["categoria"] . "</td>";
+                            echo "<td ><a class='btn btn-primary' href='${path}/ImportFileController/descargarArchivo/" . $archivo["ruta"] . "'>Descargar</a></td>"; 
+                            echo "</tr>";
+                        }
+                    ?>
+                    
+                    </tbody>
+                </table>
+            </div>
+        </div>
