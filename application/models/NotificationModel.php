da9a23f0bfe2223be40488f584be2dfab2e0747a
Alberto Diaz <teddy_liar935@hotmail.com>
2020-08-07 00:44:46 -0500

    Notification Model added

---
+++ application/models/NotificationModel.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class NotificationModel extends CI_Model {
+    private $_idNotification;
+    private $_notificationDate;
+    private $_description;
+    private $_idPerson;
+
+    public function getidNotification(){
+        return $this->_idNotifica
+    }
+
+    public public function setIdNotification()
+    {
+        
+    }
+
+
+  
+}
