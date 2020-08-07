b56c011f75d36956d6c15071e66d23faef188d62
bocanegra01 <jose.bocanegra.cbtis195.a@gmail.com>
2020-08-06 21:10:24 -0500

    FIX BUGS

--- application/views/EquipmentView.php
+++ application/views/EquipmentView.php
@@ -2,18 +2,17 @@
     <div class="col-md-12">
         <div class="box">
             <div class="box-header with-border">
-                <h1 class="box-title">Equipos
-                </h1>
+                <h1 class="box-title">Equipos</h1>
                 <div class="card">
                     <div class="card-body">
                         <form class="needs-validation" novalidate>
                             <div class="form-row">
                                 <div class="col-md-4 mb-3">
                                     <label for="validationCustom01">Nombre</label>
-                                    <input type="text" class="form-control" id="validationCustom01"
-                                        placeholder="Nombre"  required>
+                                    <input type="text" class="form-control" id="validationCustom01" placeholder="Nombre"
+                                        required>
                                     <div class="invalid-feedback">
-										Completa Nombre.
+                                        Completa Nombre.
                                     </div>
                                 </div>
                                 <div class="col-md-8 mb-3">
@@ -21,7 +20,7 @@
                                     <input type="text" class="form-control" id="validationCustom02"
                                         placeholder="Descripción" required>
                                     <div class="invalid-feedback">
-										Completa Descripción.
+                                        Completa Descripción.
                                     </div>
                                 </div>
                             </div>
@@ -31,7 +30,7 @@
                                     <input type="text" class="form-control" id="validationCustom03" placeholder="Marca"
                                         required>
                                     <div class="invalid-feedback">
-										Completa Marca.
+                                        Completa Marca.
                                     </div>
                                 </div>
                                 <div class="col-md-2 mb-3">
@@ -39,7 +38,7 @@
                                     <input type="text" class="form-control" id="validationCustom03" placeholder="Modelo"
                                         required>
                                     <div class="invalid-feedback">
-										Completa Modelo.
+                                        Completa Modelo.
                                     </div>
                                 </div>
                                 <div class="col-md-4 mb-3">
@@ -57,10 +56,10 @@
                                 </div>
                                 <div class="col-md-4 mb-3">
                                     <label for="validationCustom04">Stock</label>
-                                    <input type="number" class="form-control" id="validationCustom04" placeholder="Stock"
-                                        required>
+                                    <input type="number" class="form-control" id="validationCustom04"
+                                        placeholder="Stock" required>
                                     <div class="invalid-feedback">
-											Completa Stock.
+                                        Completa Stock.
                                     </div>
                                 </div>
                             </div>
@@ -70,7 +69,7 @@
                                     <input type="date" class="form-control" id="validationCustom05" placeholder="Fecha"
                                         required>
                                     <div class="invalid-feedback">
-									Completa Fecha de Adquisición.
+                                        Completa Fecha de Adquisición.
                                     </div>
                                 </div>
 
@@ -78,9 +77,9 @@
                                     <label for="validationCustom05">Mantenimiento</label>
                                     <select name="" class="custom-select">
                                         <option selected="Mantenimiento"></option>
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
+                                        <option value="1">Tipo 1</option>
+                                        <option value="2">Tipo 2</option>
+                                        <option value="3">Tipo 3</option>
                                     </select>
                                 </div>
                             </div>
@@ -88,57 +87,56 @@
                                 <button class="btn btn-primary" type="submit">Guardar</button>
                             </div>
                         </form>
+                    </div>
+                </div>
 
-                        <script>
-                        
-                        (function() {
-                            'use strict';
-                            window.addEventListener('load', function() {
-                                
-                                var forms = document.getElementsByClassName('needs-validation');
-                                
-                                var validation = Array.prototype.filter.call(forms, function(form) {
-                                    form.addEventListener('submit', function(event) {
-                                        if (form.checkValidity() === false) {
-                                            event.preventDefault();
-                                            event.stopPropagation();
-                                        }
-                                        form.classList.add('was-validated');
-                                    }, false);
-                                });
+                <script>
+                (function() {
+                    'use strict';
+                    window.addEventListener('load', function() {
+
+                        var forms = document.getElementsByClassName('needs-validation');
+
+                        var validation = Array.prototype.filter.call(forms, function(form) {
+                            form.addEventListener('submit', function(event) {
+                                if (form.checkValidity() === false) {
+                                    event.preventDefault();
+                                    event.stopPropagation();
+                                }
+                                form.classList.add('was-validated');
                             }, false);
-                        })();
-                        </script>
-                        <table class="table table-hover">
-                            <thead class="thead-dark" >
+                        });
+                    }, false);
+                })();
+                </script>
+
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table table-bordered table-hover">
+                            <thead class="thead-dark">
                                 <tr>
                                     <th scope="col">Nombre</th>
                                     <th scope="col">Descripción</th>
                                     <th scope="col">Marca</th>
                                     <th scope="col">Modelo</th>
-									<th scope="col">Costo</th>
+                                    <th scope="col">Costo</th>
                                     <th scope="col">Stock</th>
-                                    <th scope="col">Fecha de Adquisición</th>
+                                    <th scope="col">Fecha de adquisición</th>
                                     <th scope="col">Mantenimiento</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <th scope="row">1</th>
                                     <td></td>
                                     <td></td>
                                     <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
                                     <td></td>
                                     <td></td>
                                     <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td colspan="2"></td>
+                                    <td></td>
                                     <td></td>
                                 </tr>
                             </tbody>
                         </table>
+                    </div>
+                </div>
