0b5836183eef60b11155ef1aaca51ea0768456e9
bocanegra01 <jose.bocanegra.cbtis195.a@gmail.com>
2020-08-08 04:10:50 -0500

    CALENDAR

--- application/controllers/CalendarController.php
+++ application/controllers/CalendarController.php
@@ -3,11 +3,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class CalendarController extends CI_Controller {
 
+	function __construct() { 
+		parent::__construct();
+		$this->load->model("CalendarModel"); 
+		log_message('debug', 'MyController initialised'); 
+	} 
+
 	public function index() {
+		$data = array(
+			"events" => $this->CalendarModel->queryAllEvents()
+		);
 		$this->load->view("components/LoaderComponent");
 		$this->load->view("components/HeaderComponent");
 		$this->load->view("components/NavbarComponent");
-		$this->load->view("CalendarView");		
+		$this->load->view("CalendarView", $data);		
 		$this->load->view("components/FooterComponent");
+	}
+
+	public function validateFormData() {
+		$date = $this->input->post("fecha");
+		$event = $this->input->post("evento");
+		$importantDate = $this->input->post("fecha_importante");
+		if ($date != null and $event != null and $importantDate != null) {
+			$this->processFormData($date, $event, $importantDate);
+		} else {
+			redirect("CalendarController");
+		}
+	}
+
+	private function processFormData($date, $event, $importantDate) {
+		$this->CalendarModel->collectFormData($date, $event, $importantDate); 
+		redirect("CalendarController");
+	}
+
+	public function processDeleteEvent($idEvent) {
+		$this->CalendarModel->deleteEvent($idEvent); 
+		redirect("CalendarController");
 	}
 }
--- application/views/CalendarView.php
+++ application/views/CalendarView.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="card">
             <div class="card-body">
-                <form action="" method="POST"
+                <form action="<?php echo site_url()?>/CalendarController/validateFormData" method="POST"
                     enctype="multipart/form-data" class="mt-3 form-horizontal">
                     <div class="form-group row">
                         <label for="inputHorizontalDnger" class="col-sm-3 col-form-label">Evento</label>
@@ -23,29 +23,28 @@
                     <div class="form-group row">
                         <label for="inputHorizontalDnger" class="col-sm-3 col-form-label">Fecha importante</label>
                         <div class="col-sm-9">
-                            <select name="name" class="custom-select">
+                            <select name="fecha_importante" class="custom-select">
                                 <option selected=""></option>
-                                <option value="Si">Si</option>
-                                <option value="No">No</option>
+                                <option value="1">Si</option>
+                                <option value="0">No</option>
                             </select>
                         </div>
                     </div>
-
+                    <hr>
+                    <button id="btn-add" class="btn btn-outline-success">Agregar</button>
+                    <button id="btn-drop" class="btn btn-danger">Eliminar</button>
+                    <hr>
                     <table class="table table-bordered">
                         <thead>
                             <tr class="text-center">
-                                <th scope="col" width="25%">
-                                    <button id="btn-add" class="btn btn-outline-info">Agregar</button>
-                                    <button class="btn btn-danger" disabled>Eliminar</button>
-                                </th>
-                                <th scope="col">Personal</th>
+                                <th scope="col">Nombre de la persona</th>
                                 <th scope="col">Puesto</th>
                             </tr>
                         </thead>
                         <tbody id="tbl-box" class="text-center"></tbody>
                     </table>
                     <div class="form-group text-center">
-                        <button type="submit" class="btn btn-outline-info">Guardar</button>
+                        <button type="submit" class="btn btn-outline-info" disabled>Guardar</button>
                     </div>
                 </form>
             </div>
@@ -58,14 +57,63 @@
                             <th scope="col">Evento</th>
                             <th scope="col">Fecha</th>
                             <th scope="col">Fecha importante</th>
-                            <th scope="col">Personas</th>
+                            <th scope="col"></th>
                         </tr>
                     </thead>
                     <tbody>
-                    </tbody>
+                        <?php 
+                            $path = site_url();
+                            foreach ($events as $event) {
+                                echo "<tr >";
+                                echo "<td >" . $event["evento"] . "</td>";
+                                echo "<td >" . $event["fecha"] . "</td>";
+                                echo "<td >" . $isImportant = $event["fecha_importe"] ? "Si" : "No" . "</td>";
+                                echo "<td class='text-center'>
+                                        <a class='btn btn-outline-info disabled' href='${path}/CalendarController/processDeleteEvent/" . $event["id_calendario"] . "'>&nbsp;&nbsp;Editar&nbsp;&nbsp;</a>
+                                        <a class='btn btn-danger'  href='${path}/CalendarController/processDeleteEvent/" . $event["id_calendario"] . "'>Eliminar</a>
+                                      </td>"; 
+                                echo "</tr>";
+                            }
+                        ?>
+                        </body>
                 </table>
             </div>
-<script type="text/javascript">
-   
+        </div>
 
-</script>
+        <script>
+        const buttonAdd = document.getElementById("btn-add");
+        const tableBody = document.getElementById("tbl-box");
+        const buttonDrop = document.getElementById("btn-drop");
+
+        let jsons = [{
+                "name": "Persona 1",
+            },
+            {
+                "name": "Persona 2",
+            }
+        ];
+
+        buttonAdd.addEventListener("click", (e) => {
+            e.preventDefault();
+            var row = tableBody.insertRow(0);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            let select = document.createElement("select");
+            select.setAttribute("id", "select-input");
+            select.classList.add("form-control");
+            document.body.appendChild(select);
+
+            for (let entry of jsons) {
+                let option = document.createElement("option");
+                let t = document.createTextNode(entry["name"]);
+                option.appendChild(t);
+                cell1.appendChild(select);
+                document.getElementById("select-input").appendChild(option);
+            }
+        });
+
+        buttonDrop.addEventListener("click", (e) => {
+            e.preventDefault();
+            tableBody.deleteRow(0);
+        });
+        </script>
