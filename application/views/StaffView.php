3dc1ecd3c459d08698c3bba8a51493feaef1687f
EricAdolfoGomezRomo <61770540+EricAdolfoGomezRomo@users.noreply.github.com>
2020-08-09 18:59:34 -0500

    Metodos de guardar y eliminar

--- application/views/StaffView.php
+++ application/views/StaffView.php
@@ -6,11 +6,12 @@
                 </h1>
                 <div class="card">
                     <div class="card-body">
-                        <form class="needs-validation" novalidate>
+                        <form class="needs-validation" novalidate 
+                        action="<?php echo site_url()?>/StaffController/AgregarPersonal" method="POST">
                             <div class="form-row">
                                 <div class="col-md-4 mb-3">
                                     <label for="validationCustom01">Nombre</label>
-                                    <input type="text" class="form-control" id="validationCustom01" placeholder="Nombre"
+                                    <input type="text" name="name" class="form-control" id="validationCustom01" placeholder="Nombre"
                                         required>
                                     <div class="invalid-feedback">
                                         Completa Nombre.
@@ -18,7 +19,7 @@
                                 </div>
                                 <div class="col-md-3 mb-3">
                                     <label for="validationCustom02">Apellido Paterno</label>
-                                    <input type="text" class="form-control" id="validationCustom02"
+                                    <input type="text" name="last_name" class="form-control" id="validationCustom02"
                                         placeholder="Apellido Paterno" required>
                                     <div class="invalid-feedback">
                                         Completa Apellido Paterno.
@@ -26,7 +27,7 @@
                                 </div>
                                 <div class="col-md-3 mb-3">
                                     <label for="validationCustom02">Apellido Materno</label>
-                                    <input type="text" class="form-control" id="validationCustom02"
+                                    <input type="text" name="mother_last_name" class="form-control" id="validationCustom02"
                                         placeholder="Apellido Materno" required>
                                     <div class="invalid-feedback">
                                         Completa Apellido Materno.
@@ -34,7 +35,7 @@
                                 </div>
                                 <div class="col-md-2 mb-3">
                                     <label for="validationCustom02">Edad</label>
-                                    <input type="number" class="form-control" id="validationCustom02" placeholder="Edad"
+                                    <input type="number" name="age" class="form-control" id="validationCustom02" placeholder="Edad"
                                         required>
                                     <div class="invalid-feedback">
                                         Completa Edad.
@@ -44,7 +45,7 @@
                             <div class="form-row">
                                 <div class="col-md-2 mb-3">
                                     <label for="validationCustom02">Fecha de Nacimiento</label>
-                                    <input type="date" class="form-control" id="validationCustom03"
+                                    <input type="date" name="birthdate" class="form-control" id="validationCustom03"
                                         placeholder="Fecha de nacimiento" required>
                                     <div class="invalid-feedback">
                                         Completa Fecha de Nacimiento.
@@ -52,7 +53,7 @@
                                 </div>
                                 <div class="col-md-2 mb-3">
                                     <label for="validationCustom03">RFC</label>
-                                    <input type="text" class="form-control" id="validationCustom03" placeholder="RFC"
+                                    <input type="text" name="RFC" class="form-control" id="validationCustom03" placeholder="RFC"
                                         required>
                                     <div class="invalid-feedback">
                                         Completa RFC.
@@ -60,7 +61,7 @@
                                 </div>
                                 <div class="col-md-4 mb-3">
                                     <label for="validationCustom02">Fecha de Ingreso</label>
-                                    <input type="date" class="form-control" id="validationCustom03"
+                                    <input type="date" name="in_Date" class="form-control" id="validationCustom03"
                                         placeholder="Fecha de Ingreso" required>
                                     <div class="invalid-feedback">
                                         Completa Fecha de Ingreso.
@@ -68,7 +69,7 @@
                                 </div>
                                 <div class="col-md-4 mb-3">
                                     <label for="validationCustom02">Fecha de Egreso</label>
-                                    <input type="date" class="form-control" id="validationCustom03"
+                                    <input type="date" name="egress_Date" class="form-control" id="validationCustom03"
                                         placeholder="Fecha de Egreso" required>
                                     <div class="invalid-feedback">
                                         Completa Fecha de Egreso.
@@ -78,7 +79,7 @@
                             <div class="form-row">
                                 <div class="col-md-4 mb-3">
                                     <label for="validationCustom04">Usuario</label>
-                                    <input type="text" class="form-control" id="validationCustom04"
+                                    <input type="text" name="user" class="form-control" id="validationCustom04"
                                         placeholder="Usuario" required>
                                     <div class="invalid-feedback">
                                         Completa Stock.
@@ -86,7 +87,7 @@
                                 </div>
                                 <div class="col-md-4 mb-3">
                                     <label for="validationCustom04">Contraseña</label>
-                                    <input type="text" class="form-control" id="validationCustom04"
+                                    <input type="text" name="password" class="form-control" id="validationCustom04"
                                         placeholder="Contraseña" required>
                                     <div class="invalid-feedback">
                                         Completa Stock.
@@ -94,7 +95,7 @@
                                 </div>
                                 <div class="col-md-4 mb-3">
                                     <label for="validationCustom04">Teléfono</label>
-                                    <input type="number" class="form-control" id="validationCustom04"
+                                    <input type="number" name="phone" class="form-control" id="validationCustom04"
                                         placeholder="Teléfono" required>
                                     <div class="invalid-feedback">
                                         Completa Teléfono.
@@ -104,7 +105,7 @@
                             <div class="form-row">
                                 <div class="col-md-4 mb-3">
                                     <label for="validationCustom04">Calle</label>
-                                    <input type="text" class="form-control" id="validationCustom04" placeholder="Calle"
+                                    <input type="text" name="street" class="form-control" id="validationCustom04" placeholder="Calle"
                                         required>
                                     <div class="invalid-feedback">
                                         Completa Calle.
@@ -112,7 +113,7 @@
                                 </div>
                                 <div class="col-md-4 mb-3">
                                     <label for="validationCustom04">Fraccionamiento</label>
-                                    <input type="text" class="form-control" id="validationCustom04"
+                                    <input type="text" name="fracc" class="form-control" id="validationCustom04"
                                         placeholder="Fraccionamiento" required>
                                     <div class="invalid-feedback">
                                         Completa Fraccionamiento.
@@ -120,7 +121,7 @@
                                 </div>
                                 <div class="col-md-4 mb-3">
                                     <label for="validationCustom04">Código Postal</label>
-                                    <input type="text" class="form-control" id="validationCustom04"
+                                    <input type="text" name="postal_Code" class="form-control" id="validationCustom04"
                                         placeholder="Código Postal" required>
                                     <div class="invalid-feedback">
                                         Completa Código Postal.
@@ -134,7 +135,7 @@
                                         <div class="input-group-prepend">
                                             <span class="input-group-text" id="inputGroupPrepend">#</span>
                                         </div>
-                                        <input type="number" class="form-control" id="validationCustomUsername"
+                                        <input type="number" name="number" class="form-control" id="validationCustomUsername"
                                             placeholder="Número" aria-describedby="inputGroupPrepend" required>
                                         <div class="invalid-feedback">
                                             Completa Número.
@@ -142,8 +143,8 @@
                                     </div>
                                 </div>
                                 <div class="col-md-4 mb-3">
-                                    <label for="validationCustom04">País</label>
-                                    <input type="text" class="form-control" id="validationCustom04" placeholder="País"
+                                    <label for="validationCu"stom04">País</label>
+                                    <input type="text" name="country" class="form-control" id="validationCustom04" placeholder="País"
                                         required>
                                     <div class="invalid-feedback">
                                         Completa Código País.
@@ -152,7 +153,7 @@
 
                                 <div class="col-md-3 mb-3">
                                     <label for="validationCustom05">Experiencia</label>
-                                    <select name="" class="custom-select">
+                                    <select name="experience" class="custom-select">
                                         <option selected="Experiencia"></option>
                                         <option value="ALTA">ALTA</option>
                                         <option value="MEDIA">MEDIA</option>
@@ -162,14 +163,14 @@
                                 <div class="col-md-1 mb-3 d-flex"
                                     style="flex-direction:column;justify-content: flex-end;">
                                     <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                        <input type="checkbox" name="status" class="form-check-input" id="exampleCheck1">
                                         <label class="form-check-label" for="exampleCheck1">Estatus</label>
                                     </div>
                                 </div>
                                 <div class="form-row">
                                     <div class="col-md-6 mb-3">
                                         <label for="validationCustom02">Fecha de Creación</label>
-                                        <input type="date" class="form-control" id="validationCustom03"
+                                        <input type="date" name="creation_date" class="form-control" id="validationCustom03"
                                             placeholder="Fecha de Creación" required>
                                         <div class="invalid-feedback">
                                             Completa Fecha de Creación.
@@ -177,7 +178,7 @@
                                     </div>
                                     <div class="col-md-6 mb-3">
                                         <label for="validationCustom02">Fecha de Actualización</label>
-                                        <input type="date" class="form-control" id="validationCustom03"
+                                        <input type="date" name="update_date" class="form-control" id="validationCustom03"
                                             placeholder="Fecha de Actualización" required>
                                         <div class="invalid-feedback">
                                             Completa Fecha de Actualización.
@@ -186,7 +187,7 @@
                                 </div>
                             </div>
                             <div class="form-group text-center">
-                                <button class="btn btn-primary" type="submit">Guardar</button>
+                                <button class="btn btn-primary" type="submit" >Guardar</button>
                             </div>
                         </form>
 
@@ -214,6 +215,7 @@
                                 <table class="table table-striped table-bordered table-hover">
                                     <thead class="thead-dark text-center">
                                         <tr>
+                                            <th></th>
                                             <th scope="col">Nombre</th>
                                             <th scope="col">Apellido Paterno</th>
                                             <th scope="col">Apellido Materno</th>
@@ -238,31 +240,37 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td scope="row"></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td><i class="fas fa-pencil-alt mr-2" type="button" data-toggle="modal"
-                                                    data-target="#signup-modal"></i><i class="fas fa-trash"
-                                                    id="btn_delete"></i></td>
-                                        </tr>
+                                    <?php 
+                                        $path = site_url();
+                                        foreach ($personal as $person) {
+                                            echo "<tr >";
+                                            echo "<td class='text-center'>
+                                                <a class='btn btn-outline-info disabled' href='${path}/StaffController/processEditAdministrador/" . $person["id_personal"] . "'>&nbsp;&nbsp;Editar&nbsp;&nbsp;</a>
+                                                <a class='btn btn-danger' href='${path}/StaffController/processDeleteStaff/" . $person["id_personal"] . "'>Eliminar</a>
+                                                </td>";
+                                            echo "<td >" . $person["nombre"] . "</td>";
+                                            echo "<td >" . $person["apellido_paterno"] . "</td>";
+                                            echo "<td >" . $person["apellido_materno"] . "</td>";
+                                            echo "<td >" . $person["edad"] . "</td>";
+                                            echo "<td >" . $person["fecha_nacimiento"] . "</td>";
+                                            echo "<td >" . $person["RFC"] . "</td>";
+                                            echo "<td >" . $person["fecha_ingreso"] . "</td>";
+                                            echo "<td >" . $person["fecha_egreso"] . "</td>";
+                                            echo "<td >" . $person["usuario"] . "</td>";
+                                            echo "<td >" . $person["contrasena"] . "</td>";
+                                            echo "<td >" . $person["telefono"] . "</td>";
+                                            echo "<td >" . $person["calle"] . "</td>";
+                                            echo "<td >" . $person["fraccionamiento"] . "</td>";
+                                            echo "<td >" . $person["codigo_postal"] . "</td>";
+                                            echo "<td >" . $person["numero"] . "</td>";
+                                            echo "<td >" . $person["pais"] . "</td>";
+                                            echo "<td >" . $person["experiencia"] . "</td>";
+                                            echo "<td >" . $person["estatus"] . "</td>";
+                                            echo "<td >" . $person["fecha_creacion"] . "</td>";
+                                            echo "<td >" . $person["fecha_actualizacion"] . "</td>";
+                                            echo "</tr>";
+                                        }
+                                    ?>
                                     </tbody>
 
                                 </table>
