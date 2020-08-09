6a8307f5498c66760649a5cd388f31bd03182cd0
bocanegra01 <jose.bocanegra.cbtis195.a@gmail.com>
2020-08-09 16:43:23 -0500

    FLUJO DE ASIETNCIA TERMINADO

--- application/controllers/AssistanceController.php
+++ application/controllers/AssistanceController.php
@@ -3,15 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class AssistanceController extends CI_Controller {
 
-	private $idPerson;
-
 	function __construct() {
 		parent::__construct();
 		$this->load->model("AssistanceModel");
+		$this->load->model("PersonalModel");
 	}
 
 	public function index() {
-		$data["idPerson"] = $this->idPerson;
+		$data = array(
+			"personal" => $this->PersonalModel->queryAllPersonal()
+		);
 		$this->load->view("components/LoaderComponent");
 		$this->load->view("components/HeaderComponent");
 		$this->load->view("components/NavbarComponent");
@@ -19,17 +20,12 @@ class AssistanceController extends CI_Controller {
 		$this->load->view("components/FooterComponent");
 	}
 
-	public function setPersonId($idPerson) {
-		$this->idPerson = $idPerson;
-		$this->index();
-	}
-
 	public function takeAssistance() {
 		$schedule = $this->input->post("horario");
 		$date = $this->input->post("fecha");
 		$note = $this->input->post("nota");
 		$notAssist = $this->input->post("falto");
-		$idPerson = $this->input->post("idPerson");
+		$idPerson = $this->input->post("idPersona");
 		if ($schedule != null and $date != null and $note != null and $notAssist != null 
 				and $idPerson != null) {
 			$this->AssistanceModel->addAssistanceInfoToPerson($schedule, $date, 
--- application/views/StaffView.php
+++ application/views/StaffView.php
@@ -4,14 +4,19 @@
             <div class="box-header with-border ">
                 <h1 class="box-title font-weight-bold text-info">Toma de asistencia</h1>
                 <div class="card">
-                    <div class="card-body table-responsive">
-                    <hr>
-                        <a class='btn btn-success' href='${path}/AssistanceController/takeAssistance/" . $person["id_personal"] . "'>Lista de todas las asitencias</a>
-                    <hr>
+                    <div class="card-body table-responsive text-center">
+                        <a class="btn btn-success" href="<?php echo site_url()?>/AssistanceController">Tomar nueva
+                            asistencia</a>
+                        <hr>
+                        <div>
+                            <input type="search" id="search-input" class="form-control"
+                                placeholder="Filitar por cualquier elemento visible en la tabla"
+                                aria-controls="tbllistado">
+                        </div>
+                        <hr>
                         <table class="table table-striped table-bordered table-hover">
                             <thead class="thead-dark text-center">
                                 <tr>
-                                    <th scope="col">Acción</th>
                                     <th scope="col">Nombre</th>
                                     <th scope="col">Apellido paterno</th>
                                     <th scope="col">Apellido materno</th>
@@ -22,14 +27,11 @@
                                     <th scope="col">Nota</th>
                                 </tr>
                             </thead>
-                            <tbody>
+                            <tbody id="table-body">
                                 <?php 
                                     $path = site_url();
                                     foreach ($people as $person) {
                                         echo "<tr >";
-                                        echo "<td class='text-center'>
-                                                <a class='btn btn-outline-info' href='${path}/AssistanceController/setPersonId/" . $person["id_personal"] . "'>Tomar</a>
-                                            </td>";
                                         echo "<td >" . $person["nombre"] . "</td>";
                                         echo "<td >" . $person["apellido_paterno"] . "</td>";
                                         echo "<td >" . $person["apellido_materno"] . "</td>";
@@ -45,3 +47,13 @@
                         </table>
                     </div>
                 </div>
+                <script>
+                $(document).ready(function() {
+                    $("#search-input").on("keyup", function() {
+                        var value = $(this).val().toLowerCase();
+                        $("#table-body tr").filter(function() {
+                            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                        });
+                    });
+                });
+                </script>
