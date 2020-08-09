3c73b0b6bed87feb62ab2ce48e6c1e6832d197a5
bocanegra01 <jose.bocanegra.cbtis195.a@gmail.com>
2020-08-08 19:09:44 -0500

    CALENDAR

--- application/controllers/TrainingsController.php
+++ application/controllers/TrainingsController.php
@@ -11,3 +11,82 @@ class TrainingsController extends CI_Controller {
 		$this->load->view("components/FooterComponent");
 	}
 }
+//     public function index() {
+//         $this->load->model('TrainingsModel');
+		
+
+//         if($this->TrainingsModel->DeleteTraining(2))
+// 		{
+// 			echo "yeah";
+// 		}
+// 		else
+// 		{
+// 			echo ":'c";
+// 		}
+//     }
+	
+// 	public function NewTraining($data)
+// 	{
+// 		$this->load->model('TrainingsModel');
+// 		if($this->TrainingsModel->NewTraining($data))
+// 		{
+// 			//success view
+// 		}
+// 		else
+// 		{
+// 			//Error messesage view
+// 		}
+// 	}
+	
+// 	public function UpdateTraining($id,$data)
+// 	{
+// 		$this->load->model('TrainingsModel');
+// 		if($this->TrainingsModel->UpdateTraining($id,$data))
+// 		{
+// 			//success view
+// 		}
+// 		else
+// 		{
+// 			//Error messesage view
+// 		}
+// 	}
+	
+// 	public function AddPersonTraining($data)
+// 	{
+// 		$this->load->model('TrainingsModel');
+// 		if($this->TrainingsModel->AddPersonTraining($data))
+// 		{
+// 			//success view
+// 		}
+// 		else
+// 		{
+// 			//Error messesage view
+// 		}
+// 	}
+	
+// 	public function UpdatePersonTraining($id,$data)
+// 	{
+// 		$this->load->model('TrainingsModel');
+// 		if($this->TrainingsModel->UpdatePersonTraining($id,$data))
+// 		{
+// 			//success view
+// 		}
+// 		else
+// 		{
+// 			//Error messesage view
+// 		}
+// 	}
+	
+// 	public function DeletePersonTraining($id)
+// 	{
+// 		$this->load->model('TrainingsModel');
+// 		if($this->TrainingsModel->DeletePersonTraining($data))
+// 		{
+// 			//success view
+// 		}
+// 		else
+// 		{
+// 			//Error messesage view
+// 		}
+// 	}
+// }
