72b6c517ddb7061cc0b16f1eaf0569f5aceebd7d
bocanegra01 <jose.bocanegra.cbtis195.a@gmail.com>
2020-08-09 16:59:41 -0500

    FLUJO DE ASIETNCIA TERMINADO

--- application/views/MainView.php
+++ application/views/MainView.php
@@ -1,5 +1,17 @@
 <div class="page-wrapper">
-    <div class="container-fluid">
-        Pantalla principal, en ésta se incorpora el listado de notificaciones 
-    </div>
-</div>
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-header with-border ">
+                <h1 class="box-title font-weight-bold text-info">Notificaciones</h1>
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start activet">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1 text-muted">Notificación</h5>
+                            <small class="text-muted"><?php echo "2020/08/09" ?></small>
+                        </div>
+                        <p class="mb-1">El empleado José Guadalupe Herrada Bocanegra no asistio en día de hoy, su número
+                            de teléfono es 4491124745 para contactarlo</p>
+                    </a>
+                </div>
+            </div>
+        </div>
