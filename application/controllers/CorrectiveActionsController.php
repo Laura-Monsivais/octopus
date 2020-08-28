9033957a9414c4b21a72b09bbe2ec54d54c82fca
Miriam <miriamlunacarreon133@gmail.com>
2020-08-28 00:15:29 -0500

    Insert a la tabla acciones_correctivas
    Falta que ingrese el usuario logueado y el id_personal

--- application/controllers/CorrectiveActionsController.php
+++ application/controllers/CorrectiveActionsController.php
@@ -21,28 +21,32 @@ class CorrectiveActionsController extends CI_Controller {
 		$this->load->view("components/FooterComponent");
     }
     public function addAction(){
-        $desc_no_conf = $this->input->post('desc_no_conf', TRUE);
-        $accion_realizada = $this->input->post('accion_realizada', TRUE);
-        $c_existente = $this->input->post('c_existente', TRUE);
-        $v_ctrl_cyr = $this->input->post('v_ctrl_cyr', TRUE);
-        $monitoreo = $this->input->post('monitoreo', TRUE);
-        $problema = $this->input->post('problema', TRUE);
-        $com_proceso = $this->input->post('com_proceso', TRUE);
-        $recolec_dato = $this->input->post('recolec_dato', TRUE);
-        $analizar_dato = $this->input->post('analizar_dato', TRUE);
-        $posible_causa = $this->input->post('posible_causa', TRUE);
-        $verif_causa = $this->input->post('verif_causa', TRUE);
-        $sol_imple = $this->input->post('sol_imple', TRUE);
-        $impl_sol = $this->input->post('impl_sol', TRUE);
-        $eval_efe = $this->input->post('eval_efe', TRUE);
-        $estbl_cambio = $this->input->post('estbl_cambio', TRUE);
-        $aprend_cierre = $this->input->post('aprend_cierre', TRUE);
-        $realizado_por = $this->input->post('realizado_por', TRUE);
-        $validado_por = $this->input->post('validado_por', TRUE);
+        $datos = $this->input->post();
 
-        $this->CorrectiveActionsModel->setAcciones($desc_no_conf, $accion_realizada, $c_existente,
+        if(isset($datos)){
+            $desc_no_conf = $datos['input1'];
+            $accion_realizada = $datos['input2'];
+            $c_existente = $datos['input3'];
+            $v_ctrl_cyr = $datos['input4'];
+            $monitoreo = $datos['input5'];
+            $problema = $datos['input6'];
+            $com_proceso = $datos['input7'];
+            $recolec_dato = $datos['input8'];
+            $analizar_dato = $datos['input9'];
+            $posible_causa = $datos['input10'];
+            $verif_causa = $datos['input11'];
+            $sol_imple = $datos['input12'];
+            $impl_sol = $datos['input13'];
+            $eval_efe = $datos['input14'];
+            $estbl_cambio = $datos['input15'];
+            $aprend_cierre = $datos['input16'];
+            $realizado_por = $datos['input17'];
+            $validado_por = $datos['input18'];
+
+            $this->CorrectiveActionsModel->setAcciones($desc_no_conf, $accion_realizada, $c_existente,
             $v_ctrl_cyr, $monitoreo, $problema, $com_proceso, $recolec_dato, $analizar_dato, 
             $posible_causa, $verif_causa, $sol_imple, $sol_imple, $impl_sol, $eval_efe, $estbl_cambio, $aprend_cierre,
             $realizado_por, $validado_por);
+        }
     }
 }
--- application/views/CorrectiveActionsView.php
+++ application/views/CorrectiveActionsView.php
@@ -8,39 +8,145 @@
                         <form class="needs-validation" novalidate>
                             <div class="form-row">
 
-                <table class="table table-hover">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">Clave</th>
-                            <th scope="col">Version</th>
-                            <th scope="col">Fecha de creación</th>
-                            <th scope="col">Última actualización</th>
-                            <th scope="col">No conformidad</th>
-                            <th scope="col"><i class="fas fa-cogs"></i></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <?php 
-                        $versioness = json_decode(json_encode($versiones), true);
-                        $path = site_url();
-                        foreach ($versioness as $version) {
-                            echo "<tr >";
-                            echo "<td >" . $version["clve_accion_cor"] . "</td>";
-                            echo "<td >" . $version["v_accion_cor"] . "</td>";
-                            echo "<td >" . $version["fecha_accion_cor"] . "</td>";
-                            echo "<td >" . $version["factual_accion_cor"] . "</td>";
-                            echo "<td >" . $version["desc_no_conf"] . "</td>";
-                            echo "<td><i class='fas fa-pencil-alt mr-2' type='button' data-toggle='modal' data-target='#signup-modal' href='"echo base_url()"/index.php/CorrectiveActionsController/editAction/$version['id_accion_cor']'></i><i class='fas fa-trash' id='btn_delete'></i></td>"; 
-                            echo "</tr>";
-                        }
-                    ?>
-                    </tbody>
-                </table>
-                <i class='btn btn-primary' data-toggle='modal' data-target='#add-modal'>Añadir</i>
+                                <table class="table table-hover">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th scope="col">Clave</th>
+                                            <th scope="col">Version</th>
+                                            <th scope="col">Fecha de creación</th>
+                                            <th scope="col">Última actualización</th>
+                                            <th scope="col">No conformidad</th>
+                                            <th scope="col"><i class="fas fa-cogs"></i></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php 
+                                            $versioness = json_decode(json_encode($versiones), true);
+                                            $path = site_url();
+                                            foreach ($versioness as $version) {
+                                                echo "<tr >";
+                                                echo "<td >" . $version["clve_accion_cor"] . "</td>";
+                                                echo "<td >" . $version["v_accion_cor"] . "</td>";
+                                                echo "<td >" . $version["fecha_accion_cor"] . "</td>";
+                                                echo "<td >" . $version["factual_accion_cor"] . "</td>";
+                                                echo "<td >" . $version["desc_no_conf"] . "</td>";
+                                                echo "<td><i class='fas fa-pencil-alt mr-2' type='button' data-toggle='modal' data-target='#signup-modal'></i><i class='fas fa-trash' id='btn_delete'></i></td>"; 
+                                                echo "</tr>";
+                                            }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </form>
+                    </div>
+                </div></div></div></div></div>
+
+                <div class="page-wrapper">
+                    <div class="col-md-12">
+                        <div class="box">
+                            <div class="box-header with-border ">
+                                <h1 class="box-title font-weight-bold text-info">Procedimiento de acciones correctivas</h1>
+                                <div class="card">
+                                    <div class="card-body">
+                                        <form action="<?php echo base_url()?>index.php/CorrectiveActionsController/addAction" method="POST" >
+                                            <div class="form-group"><label><b>Paso 0:</b></label>
+                                                <div class="form-row">
+                                                    <div class="col-md-4 mb-3"><label>Descripción de la No conformidad:</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input1" rows="5"></textarea>
+                                                    </div>
+                                                    <div class="col-md-4 mb-3"><label>Acciones realizadas después del problema:</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input2" rows="5"></textarea>
+                                                    </div>
+                                                    <div class="col-md-4 mb-3"><label>Controles existentes:</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input3" rows="5"></textarea>
+                                                    </div>
+                                                </div><br>
+                                                <div class="form-row">
+                                                    <div class="col-md-6 mb-3"><label>Verificación de acciones de contención  y remediación:</label><br>
+                                                    <textarea maxlength="500" class="form-control" name="input4" rows="5"></textarea>
+                                                    </div>
+                                                    <div class="col-md-6 mb-3"><label>Monitoreo de acciones implementadas:</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input5" rows="5"></textarea>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="row">
+                                                    <div class="col-md-4 mb-3"><label><b>Paso 1:</b><br> Definir el problema</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input6" rows="5"></textarea>
+                                                    </div>
+                                                    <div class="col-md-4 mb-3"><label><b>Paso 2:</b><br> Comprender el proceso</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input7" rows="5"></textarea>
+                                                    </div>
+                                                    <div class="col-md-4 mb-3"><label><b>Paso 3:</b><br> Recolectar datos</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input8" rows="5"></textarea>
+                                                    </div>
+                                                </div><br>
+                                                <div class="row">
+                                                    <div class="col-md-4 mb-3"><label><b>Paso 4:</b><br> Analizar los datos</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input9" rows="5"></textarea>
+                                                    </div>
+                                                    <div class="col-md-4 mb-3"><label><b>Paso 5:</b><br> Identificar las posibles causas</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input10" rows="5"></textarea>
+                                                    </div>
+                                                    <div class="col-md-4 mb-3"><label><b>Paso 6:</b><br> Comprobar o verificar las causas</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input11" rows="5"></textarea>
+                                                    </div>
+                                                </div><br>
+                                                <div class="row">
+                                                    <div class="col-md-4 mb-3"><label><b>Paso 7:</b><br> Seleccionar solucion(es) a ser implementada(s)</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input12" rows="5"></textarea>
+                                                    </div>
+                                                    <div class="col-md-4 mb-3"><label><b>Paso 8:</b><br> Implementar la(s) solucion(es)</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input13" rows="5"></textarea>
+                                                    </div>
+                                                    <div class="col-md-4 mb-3"><label><b>Paso 9:</b><br> Evaluar el/los efecto(s)</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input14" rows="5"></textarea>
+                                                    </div>
+                                                </div><br>
+                                                <div class="row">
+                                                    <div class="col-md-6 mb-3"><label><b>Paso 10:</b><br> Establecer los cambios</label><br>
+                                                        <textarea maxlength="500" class="form-control" name="input15" rows="5"></textarea>
+                                                    </div>
+                                                    <div class="col-md-6 mb-3"><label><b>Paso 11:</b><br> Registrar los aprendizajes y hacer el cierre de la no conformidad</label>
+                                                        <textarea class="form-control" name="input16" rows="5"></textarea>
+                                                    </div>
+                                                    <div class="col-md-6 mb-3"><label><br> Realizado por.</label>
+                                                        <textarea class="form-control" name="input17" rows="5">
+                                                            <?php 
+                                                                //echo $this->session->userdata("usuario");
+                                                            ?>
+                                                        </textarea>
+                                                    </div>
+                                                </div><br>
+                                                <div class="row">
+                                                    <div class="col-md-12 mb-3"><label>Validado por:<br /></label><select class="form-control" name="input18">
+                                                        <optgroup label="This is a group">
+                                                            <option value="12" selected>Seleccionar opción</option>
+                                                                <?php 
+                                                                    $persona = json_decode(json_encode($personal), true);
+                                                                    $path = site_url();
+                                                                    foreach ($persona as $person) {
+                                                                        echo "<option>". $person["id_personal"] . "</option>";
+                                                                    }
+                                                                ?>
+                                                        </optgroup>
+                                                    </select></div>
+                                                </div>
+                                                <input type="submit" class="btn btn-primary" id="insertar" value="Añadir">
+                                            </div>
+                            
+                                        </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
 
 
-                <div id="add-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+                <!--<div id="add-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered modal-lg">
                         <div class="modal-content">
                             <div class="modal-header">
@@ -113,9 +219,9 @@
                                     <textarea class="form-control" id="aprend_cierre" rows="5"></textarea>
                                 </div>
                                 <div class="col-md-6 mb-3"><label><br> Realizado por.</label>
-                                    <textarea class="form-control" id="realizadopor" rows="5">
+                                    <textarea class="form-control" id="realizado_por" rows="5">
                                     <?php 
-                                        echo $this->session->userdata("usuario");
+                                        /*echo $this->session->userdata("usuario");
                                     ?>
                                     </textarea>
                                 </div>
@@ -129,18 +235,18 @@
                                                     $path = site_url();
                                                     foreach ($persona as $person) {
                                                         echo "<option>". $person["id_personal"] . "</option>";
-                                                    }
+                                                    }*/
                                                     ?>
                                             </optgroup>
                                         </select></div>
                                     </div>
-                                    
+                                <input type="submit" class="btn btn-primary" id="insertar" value="Añadir">
                             </div>
-                            <input type="submit" class="btn btn-primary" id="Guardar" value="Añadir">
+                            
                         </form>
 
                             </div>
-                        </div><!-- /.modal-content -->
+                        </div> /.modal-content -->
                     </div><!-- /.modal-dialog -->
                 </div><!-- /.modal -->
 
